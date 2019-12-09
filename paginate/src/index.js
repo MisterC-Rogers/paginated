0907a8530cb54761e625913aaa2ac51c859e832b
MisterC-Rogers <charles.rogers2024@gmail.com>
2019-12-08 18:08:52 -0600

    fixed overage

--- paginate/src/index.js
+++ paginate/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 //import styling 
 import "./styles/global.scss";
-import './index.css'
+// import './index.css'
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
